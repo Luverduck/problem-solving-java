ab26df5dfcb87f5e087d6bafbdf6ae8b120a089c
Luverduck <hyekko92@gmail.com>
2024-06-30 02:02:43 +0900

    [1330번] 두 수 비교하기

---
+++ Baekjoon/src/step01_by_level/lv02_conditional/p01_1330/Main.java
@@ -0,0 +1,24 @@
+package step01_by_level.lv02_conditional.p01_1330;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		if(a > b)
+			System.out.print(">");
+		else if(a < b)
+			System.out.print("<");
+		else
+			System.out.print("=");
+	}
+}
