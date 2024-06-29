8d9987e4990d52689f2755f71db395f90ce37090
Luverduck <hyekko92@gmail.com>
2024-06-30 01:53:59 +0900

    Java 코드 작성 템플릿

---
+++ Baekjoon/Main.java
@@ -0,0 +1,13 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public Main() {
+		// 프로젝트 하위의 fileio 폴더에서 input.txt 파일을 읽어온다.
+		System.setIn(new FileInputStream("fileio/input.txt"));
+		
+		
+	}
+}
