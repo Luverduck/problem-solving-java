20688cbb3e48c28a8b758a51204decd3c02b9760
Luverduck <hyekko92@gmail.com>
2024-07-03 21:44:06 +0900

    [10926번] ??!

---
+++ Baekjoon/src/step01_by_level/lv01_io_and_arithmetics/p07_10926/Main.java
@@ -0,0 +1,15 @@
+package Baekjoon.src.step01_by_level.lv01_io_and_arithmetics.p07_10926;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //System.setIn(new FileInputStream("fileio/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine() + "??!");
+
+        br.close();
+    }
+}
