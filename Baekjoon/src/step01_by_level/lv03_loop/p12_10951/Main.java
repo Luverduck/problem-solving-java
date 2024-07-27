74e8f2b45d1c1823d5eafab54168b1f5fd6d8a5d
Luverduck <hyekko92@gmail.com>
2024-07-27 12:20:37 +0900

    [10951번] A+B - 4

---
+++ Baekjoon/src/step01_by_level/lv03_loop/p12_10951/Main.java
@@ -0,0 +1,28 @@
+package Baekjoon.src.step01_by_level.lv03_loop.p12_10951;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        String temp;
+        while(true) {
+            temp = br.readLine();
+            if(temp == null)
+                break;
+            st = new StringTokenizer(temp);
+            int result = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+            bw.append(String.valueOf(result)).append('\n');
+        }
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
