c7224790730bdcca0de742894bd36c1ecf460460
Luverduck <hyekko92@gmail.com>
2024-08-12 00:26:21 +0900

    [9086번] 문자열

---
+++ Baekjoon/src/step01_by_level/lv05_string/p03_9086/Main.java
@@ -0,0 +1,27 @@
+package Baekjoon.src.step01_by_level.lv05_string.p03_9086;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String temp = null;
+        for(int i = 0; i < N; ++i) {
+            temp = br.readLine();
+            bw.append(temp.charAt(0));
+            bw.append(temp.charAt(temp.length() - 1));
+            bw.append('\n');
+        }
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
