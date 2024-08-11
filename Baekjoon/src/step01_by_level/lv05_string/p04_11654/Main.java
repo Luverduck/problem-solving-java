6102d37813b85673e46fbd90b27e74128009fea8
Luverduck <hyekko92@gmail.com>
2024-08-12 00:26:40 +0900

    [11654번] 아스키 코드

---
+++ Baekjoon/src/step01_by_level/lv05_string/p04_11654/Main.java
@@ -0,0 +1,20 @@
+package Baekjoon.src.step01_by_level.lv05_string.p04_11654;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        bw.append(String.valueOf(str.charAt(0) - '\0'));
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
