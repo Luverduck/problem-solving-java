7dcf18203245952a249d5bd4551440b3451428ab
Luverduck <hyekko92@gmail.com>
2024-09-05 21:19:36 +0900

    [2292번] 벌집

---
+++ Baekjoon/src/step01_by_level/lv08_math_1/p05_2292/Main.java
@@ -0,0 +1,29 @@
+package Baekjoon.src.step01_by_level.lv08_math_1.p05_2292;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int maxNo = 1;
+        int roomCnt = 1;
+        while(N > maxNo)
+        {
+            maxNo += 6 * roomCnt;
+            roomCnt++;
+        }
+
+        bw.append(String.valueOf(roomCnt));
+
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
