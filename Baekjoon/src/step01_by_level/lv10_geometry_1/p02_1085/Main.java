eb980a99ba3db467fb5aa822e471d916240b0eec
Luverduck <hyekko92@gmail.com>
2024-09-18 20:26:55 +0900

    [1085번] 직사각형에서 탈출

---
+++ Baekjoon/src/step01_by_level/lv10_geometry_1/p02_1085/Main.java
@@ -0,0 +1,30 @@
+package Baekjoon.src.step01_by_level.lv10_geometry_1.p02_1085;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int minX = Math.min(w - x, x);
+        int minY = Math.min(h - y, y);
+
+        int minDistance = Math.min(minX, minY);
+
+        bw.append(String.valueOf(minDistance));
+
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
