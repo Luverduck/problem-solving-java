b58fcb37bb59701c0f945d4daf43ce3f5fdbfc74
Luverduck <hyekko92@gmail.com>
2024-11-02 01:28:15 +0900

    [15964번] 이상한 기호

---
+++ Baekjoon/src/step00_by_ID/p15964/Main.java
@@ -0,0 +1,22 @@
+package Baekjoon.src.step00_by_ID.p15964;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long res = (A + B) * (A - B);
+        bw.append(String.valueOf(res));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
