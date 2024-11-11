762c698fe651a0ef48c35f030996dcf5aa27ba14
Luverduck <hyekko92@gmail.com>
2024-11-11 22:31:53 +0900

    [9243번] 파일 완전 삭제

---
+++ Baekjoon/src/step00_by_ID/p09243/Main.java
@@ -0,0 +1,29 @@
+package Baekjoon.src.step00_by_ID.p09243;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        char[] source = br.readLine().toCharArray();
+        char[] target = br.readLine().toCharArray();
+
+        boolean flag = true;
+        boolean isEven = N % 2 == 0;
+        for(int i = 0; i < source.length; ++i) {
+            if( (isEven && source[i] == target[i]) || (!isEven && source[i] != target[i]) ) continue;
+            flag = false;
+            break;
+        }
+        bw.append(flag ? "Deletion succeeded" : "Deletion failed");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
