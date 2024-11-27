6f70e31cc8fef0cc38dc05fb50adca82e926380a
Luverduck <hyekko92@gmail.com>
2024-11-27 21:47:57 +0900

    [9654번] 나부 함대 데이터

---
+++ Baekjoon/src/step00_by_ID/p09654/Main.java
@@ -0,0 +1,25 @@
+package Baekjoon.src.step00_by_ID.p09654;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.append(
+                "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n" +
+                "N2 Bomber      Heavy Fighter  Limited    21        \n" +
+                "J-Type 327     Light Combat   Unlimited  1         \n" +
+                "NX Cruiser     Medium Fighter Limited    18        \n" +
+                "N1 Starfighter Medium Fighter Unlimited  25        \n" +
+                "Royal Cruiser  Light Combat   Limited    4         \n"
+        );
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
