15aec1dc676a96f6e1fd89737d01c5514eb872a4
Luverduck <hyekko92@gmail.com>
2024-11-30 15:47:30 +0900

    [26082번] WARBOY

---
+++ Baekjoon/src/step00_by_ID/p26082/Main.java
@@ -0,0 +1,23 @@
+package Baekjoon.src.step00_by_ID.p26082;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        bw.append(String.valueOf(3 * C * B / A));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
