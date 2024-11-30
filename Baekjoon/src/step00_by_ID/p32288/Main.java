b44835a5ca94abb704b8c1cbf293f6f308fedf70
Luverduck <hyekko92@gmail.com>
2024-11-30 15:53:36 +0900

    [32288번] 바코드 닉네임

---
+++ Baekjoon/src/step00_by_ID/p32288/Main.java
@@ -0,0 +1,22 @@
+package Baekjoon.src.step00_by_ID.p32288;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String ID = br.readLine();
+        ID = ID.replace('l', 'L');
+        ID = ID.replace('I', 'i');
+        bw.append(ID);
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
