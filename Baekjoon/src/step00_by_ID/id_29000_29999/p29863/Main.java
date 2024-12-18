bc7b812592ae324cd205982a460393128b836fc4
Luverduck <hyekko92@gmail.com>
2024-12-19 08:51:33 +0900

    [29863번] Arno's Sleep Schedule

---
+++ Baekjoon/src/step00_by_ID/id_29000_29999/p29863/Main.java
@@ -0,0 +1,21 @@
+package Baekjoon.src.step00_by_ID.id_29000_29999.p29863;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int first = Integer.parseInt(br.readLine());
+        if(first >= 20) first -= 24;
+        int second = Integer.parseInt(br.readLine());
+        bw.append(String.valueOf(second - first));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
