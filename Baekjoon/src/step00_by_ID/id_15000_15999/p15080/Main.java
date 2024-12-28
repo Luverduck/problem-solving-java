edffbb35283f235ee7551b8ce7761a5fdedcc373
Luverduck <hyekko92@gmail.com>
2024-12-28 16:36:16 +0900

    [15080번] Every Second Counts

---
+++ Baekjoon/src/step00_by_ID/id_15000_15999/p15080/Main.java
@@ -0,0 +1,26 @@
+package Baekjoon.src.step00_by_ID.id_15000_15999.p15080;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] start = br.readLine().split(" : ");
+        String[] end = br.readLine().split(" : ");
+        int diff = 0;
+        for(int i = 0; i < 3; ++i) {
+            diff *= 60;
+            diff += Integer.parseInt(end[i]) - Integer.parseInt(start[i]);
+        }
+        if(diff < 0) diff += 24 * 60 * 60;
+        bw.append(String.valueOf(diff));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
