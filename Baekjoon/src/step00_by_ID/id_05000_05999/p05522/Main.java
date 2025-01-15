528f42c0e6a40a07b58212cf35affa8b63223e57
Luverduck <hyekko92@gmail.com>
2025-01-16 05:21:18 +0900

    [5522번] 카드 게임

---
+++ Baekjoon/src/step00_by_ID/id_05000_05999/p05522/Main.java
@@ -0,0 +1,21 @@
+package Baekjoon.src.step00_by_ID.id_05000_05999.p05522;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int res = 0;
+        for(int i = 0; i < 5; ++i)
+            res += Integer.parseInt(br.readLine());
+        bw.append(String.valueOf(res));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
