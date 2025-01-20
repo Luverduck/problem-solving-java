ac1aef753842793c9bedc4b5c9edd0bafda90943
Luverduck <hyekko92@gmail.com>
2025-01-20 21:16:31 +0900

    [8387번] Dyslexia

---
+++ Baekjoon/src/step00_by_ID/id_08000_08999/p08387/Main.java
@@ -0,0 +1,26 @@
+package Baekjoon.src.step00_by_ID.id_08000_08999.p08387;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        char[] origin = br.readLine().toCharArray();
+        char[] rewritten = br.readLine().toCharArray();
+        int res = 0;
+        for(int i = 0; i < n; ++i) {
+            if(origin[i] != rewritten[i]) continue;
+            res++;
+        }
+        bw.append(String.valueOf(res));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
