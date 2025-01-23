96ef19f19da035e4b33c5b5d639fca12f3f684b2
Luverduck <hyekko92@gmail.com>
2025-01-23 22:01:56 +0900

    [25881번] Electric Bill

---
+++ Baekjoon/src/step00_by_ID/id_25000_25999/p25881/Main.java
@@ -0,0 +1,26 @@
+package Baekjoon.src.step00_by_ID.id_25000_25999.p25881;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int baseRate = Integer.parseInt(st.nextToken());
+        int additionalRate = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; ++i) {
+            int KWH = Integer.parseInt(br.readLine());
+            int price = KWH < 1000 ? KWH * baseRate : 1000 * baseRate + (KWH - 1000) * additionalRate;
+            bw.append(String.valueOf(KWH)).append(' ').append(String.valueOf(price)).append('\n');
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
