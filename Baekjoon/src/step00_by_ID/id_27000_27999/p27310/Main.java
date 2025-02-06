643814cf202fd9de2eff07ee5b383b8cb882ae22
Luverduck <hyekko92@gmail.com>
2025-02-06 15:46:35 +0900

    [27310번] :chino_shock:

---
+++ Baekjoon/src/step00_by_ID/id_27000_27999/p27310/Main.java
@@ -0,0 +1,24 @@
+package Baekjoon.src.step00_by_ID.id_27000_27999.p27310;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String emoji = br.readLine();
+        int res = emoji.length();
+        for(int i = 0; i < emoji.length(); ++i) {
+            if(emoji.charAt(i) == ':') res++;
+            if(emoji.charAt(i) == '_') res += 5;
+        }
+        bw.append(String.valueOf(res));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
