53778cb507ebfa9192dd70370fa65b16cab325bb
Luverduck <hyekko92@gmail.com>
2025-02-11 19:40:20 +0900

    [23278번] 영화 평가

---
+++ Baekjoon/src/step00_by_ID/id_23000_23999/p23278/Main.java
@@ -0,0 +1,30 @@
+package Baekjoon.src.step00_by_ID.id_23000_23999.p23278;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+
+        String[] input = br.readLine().split(" ");
+
+        int[] scores = new int[N];
+        for(int i = 0; i < N; ++i)
+            scores[i] = Integer.parseInt(input[i]);
+        Arrays.sort(scores);
+
+        double total = 0;
+        for(int i = L; i < N - H; ++i)
+            total += scores[i];
+        System.out.println(total / (N - (L + H)));
+
+        br.close();
+    }
+}
