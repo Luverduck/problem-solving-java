50e4a554571010f9007c64cb82e7e71626fcddbe
Luverduck <hyekko92@gmail.com>
2025-03-09 20:00:41 +0900

    [19946번] 2의 제곱수 계산하기

---
+++ Baekjoon/src/step00_by_ID/id_19000_19999/p19946/Main.java
@@ -0,0 +1,24 @@
+package Baekjoon.src.step00_by_ID.id_19000_19999.p19946;
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        BigInteger N = new BigInteger(br.readLine());
+        int res = 64;
+        while(N.mod(BigInteger.TWO).equals(BigInteger.ZERO)) {
+            N = N.divide(BigInteger.TWO);
+            res--;
+        }
+        bw.append(String.valueOf(res));
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
