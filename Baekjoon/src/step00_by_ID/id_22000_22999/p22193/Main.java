ec4d5c0b1ffe5ae74d05c1082e2a8ada8b7c663f
Luverduck <hyekko92@gmail.com>
2025-03-11 00:58:03 +0900

    [22193번] Multiply

---
+++ Baekjoon/src/step00_by_ID/id_22000_22999/p22193/Main.java
@@ -0,0 +1,22 @@
+package Baekjoon.src.step00_by_ID.id_22000_22999.p22193;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.readLine();
+        BigInteger A = new BigInteger(br.readLine());
+        BigInteger B = new BigInteger(br.readLine());
+        bw.append(A.multiply(B).toString());
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
