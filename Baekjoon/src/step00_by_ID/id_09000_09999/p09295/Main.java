3569ad4c9be3f3e57f9bfcb66f7ede42137afe0a
Luverduck <hyekko92@gmail.com>
2025-03-12 01:21:57 +0900

    [9295번] 주사위

---
+++ Baekjoon/src/step00_by_ID/id_09000_09999/p09295/Main.java
@@ -0,0 +1,26 @@
+package Baekjoon.src.step00_by_ID.id_09000_09999.p09295;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int t = 1; t <= T; ++t) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append("Case ").append(t).append(": ").append(A + B).append('\n');
+        }
+        bw.append(sb.toString());
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
