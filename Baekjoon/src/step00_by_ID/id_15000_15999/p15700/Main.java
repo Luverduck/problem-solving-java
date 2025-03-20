53a0f33e4a815cd479627168f3409060b271ef0d
Luverduck <hyekko92@gmail.com>
2025-03-21 03:12:53 +0900

    [15700번] 타일 채우기 4

---
+++ Baekjoon/src/step00_by_ID/id_15000_15999/p15700/Main.java
@@ -0,0 +1,21 @@
+package Baekjoon.src.step00_by_ID.id_15000_15999.p15700;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+        bw.append(String.valueOf((N * M) / 2));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
