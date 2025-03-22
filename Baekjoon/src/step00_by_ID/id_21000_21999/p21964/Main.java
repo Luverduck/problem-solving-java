010494fa178102f999400f09dca3d5e6930297e9
Luverduck <hyekko92@gmail.com>
2025-03-23 05:11:29 +0900

    [21964번] 선린인터넷고등학교 교가

---
+++ Baekjoon/src/step00_by_ID/id_21000_21999/p21964/Main.java
@@ -0,0 +1,22 @@
+package Baekjoon.src.step00_by_ID.id_21000_21999.p21964;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+        for(int n = 0; n < N; ++n)
+            if(n >= N - 5)
+                bw.append(S.charAt(n));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
