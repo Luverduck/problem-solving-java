10118f6d50eff68c4ee3763e0ec07d6c6cb04d5b
Luverduck <hyekko92@gmail.com>
2025-03-30 16:01:39 +0900

    [15829번] Hashing

---
+++ Baekjoon/src/step00_by_ID/id_15000_15999/p15829/Main.java
@@ -0,0 +1,27 @@
+package Baekjoon.src.step00_by_ID.id_15000_15999.p15829;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.readLine();
+        String S = br.readLine();
+
+        long res = 0;
+        long pow = 1;
+        for(char c : S.toCharArray()) {
+            res += (c - 'a' + 1) * pow;
+            pow = (pow * 31L) % 1234567891L;
+        }
+        bw.append(String.valueOf(res % 1234567891L));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
