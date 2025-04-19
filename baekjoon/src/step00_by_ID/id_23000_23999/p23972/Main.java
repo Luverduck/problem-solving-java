9e10de27a106a7c01e3fecbca1e7d797c6d645c5
Luverduck <hyekko92@gmail.com>
2025-04-20 04:19:45 +0900

    [23972번] 악마의 제안

---
+++ baekjoon/src/step00_by_ID/id_23000_23999/p23972/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.src.step00_by_ID.id_23000_23999.p23972;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long K = Long.parseLong(st.nextToken());
+        long N = Long.parseLong(st.nextToken());
+        if(N == 1) {
+            bw.append("-1");
+        } else {
+            long res = K * N / (N - 1L);
+            if(K * N % (N - 1L) != 0) res++;
+            bw.append(String.valueOf(res));
+        }
+
+        // in = (in - K) * N
+        // in * (N - 1) = K * N
+        // in = K * N / (N - 1)
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
