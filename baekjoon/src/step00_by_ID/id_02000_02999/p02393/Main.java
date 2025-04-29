346cabb15b04720966baa3a2720fde129a9d0d7a
Luverduck <hyekko92@gmail.com>
2025-04-30 01:12:44 +0900

    [2393번] Rook

---
+++ baekjoon/src/step00_by_ID/id_02000_02999/p02393/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.src.step00_by_ID.id_02000_02999.p02393;
+
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.append("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
